5ad207a21908d3b8a642fb86a0c9685116c6f00b
barleegrow <63936462+barleegrow@users.noreply.github.com>
2020-04-24 13:12:15 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,14 @@
-document.getElementById("date").innerhtml = "Last Updated" + document.lastModified;
+/*document.getElementById("date").innerhtml = "Last Updated" + document.lastModified;
 var date = new Date ();
-document.getElementById("year").innerhtml = date.getFullYear();
+document.getElementById("year").innerhtml = date.getFullYear();*/
+    newFunction();
+function newFunction() {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById("date")
+        .textContent = new Date().toLocaleDateString("en-US", options);
+}
