6b572cb98e411b47b2784c73167ed133ee307475
barleegrow <bstacey@rta.org>
2020-05-14 15:22:35 -0400

    style

--- week4/js/week4.js
+++ week4/js/week4.js
@@ -2,9 +2,11 @@ function toggle_menu() {
     document.getElementById("main_nav").classList.toggle("nav_hidden");
  }
  const date = new Date();
- const days = ['Sunday, 'Monday;, 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
  const MonthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
  const dayName = days[date.getDay()];
  const monthName = monthName [date.getMonth()];
+ const year = date.getFullYear();
  document.getElementById("today").innerHTML = dayName +', ' + date.getDate() + ' ' + monthName + ' ' + year;
- document.getElementById("nave_main).classlist.toggle("nav_hidden);
+ document.getElementById("year").innerHTML = year;
+ document.getElementById("nav_main").classlist.toggle("nav_hidden");
