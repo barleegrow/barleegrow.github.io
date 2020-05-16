52bb177e5fd740ac35f47280eb45e3eba0d01b20
barleegrow <bstacey@rta.org>
2020-05-16 16:12:25 -0400

    javascript fix

--- week4/js/week4.js
+++ week4/js/week4.js
@@ -12,9 +12,10 @@ document.getElementByClassName("navigation")[0].classList.toggle("responsive");
      var dayDate = today.getDate();
      var year = today.getFullYear();
      return  day + ", " + dayDate + " " + month + " " + year;
+ }
  //document.getElementById("nav_main").classlist.toggle("nav_hidden");
 
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+//const hambutton = document.querySelector('.ham');
+//const mainnav = document.querySelector('.navigation')
 
 //hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
