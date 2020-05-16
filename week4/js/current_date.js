8125b5fa08928699d24a780c45911e9c09e6e33a
barleegrow <bstacey@rta.org>
2020-05-16 16:24:21 -0400

    js separation

---
+++ week4/js/current_date.js
@@ -0,0 +1,12 @@
+document.getElementById("modified").innerHTML = todayDate();
+
+function todayDate() {
+    var today = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    var month = months[today.getMonth()];
+    var day = days[today.getDay()];
+    var dayDate = today.getDate();
+    var year = today.getFullYear();
+    return  day + ", " + dayDate + " " + month + " " + year;
+}
