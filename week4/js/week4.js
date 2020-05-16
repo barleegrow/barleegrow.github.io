47dd63c9aa1df7f37dcc6031f2ec0c399d4af344
barleegrow <bstacey@rta.org>
2020-05-16 16:07:43 -0400

    at a loss

--- week4/js/week4.js
+++ week4/js/week4.js
@@ -1,17 +1,20 @@
 function toggleMenu() {
 document.getElementByClassName("navigation")[0].classList.toggle("responsive");
  }
- const date = new Date();
- const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
- const MonthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
- const dayName = days[date.getDay()];
- const monthName = monthName [date.getMonth()];
- const year = date.getFullYear();
- document.getElementById("today").innerHTML = dayName +', ' + date.getDate() + ' ' + monthName + ' ' + year;
- document.getElementById("year").innerHTML = year;
+ document.getElementById("modified").innerHTML = todayDate();
+
+ function todayDate() {
+     var today = new Date();
+     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+     const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+     var month = months[today.getMonth()];
+     var day = days[today.getDay()];
+     var dayDate = today.getDate();
+     var year = today.getFullYear();
+     return  day + ", " + dayDate + " " + month + " " + year;
  //document.getElementById("nav_main").classlist.toggle("nav_hidden");
 
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+//hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
