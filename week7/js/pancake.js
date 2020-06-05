cd4d27464803dbcdcaadb3712bd3d654e6611f5c
barleegrow <bstacey@rta.org>
2020-06-05 17:08:31 -0400

    clean-up

--- week7/js/pancake.js
+++ week7/js/pancake.js
@@ -2,7 +2,9 @@ const date = new Date();
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const dayName = days[date.getDay()];
 
-if(dayName == 'Friday') {
-   document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-   document.getElementById("alert").classList.add("show");
+function pancakes() {
+   if(dayName == 'Friday') {
+      document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+      document.getElementById("alert").classList.add("show");
+   }
 }
