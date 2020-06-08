dc9f1fb465df9440966e6dfa6c161a73bc3f6425
barleegrow <bstacey@rta.org>
2020-06-08 17:53:27 -0400

    week 8 housekeeping

---
+++ week8/js/pancake.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = days[date.getDay()];
+
+function pancakes() {
+   if(dayName == 'Friday') {
+      document.getElementById("alert").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+      document.getElementById("alert").classList.add("show");
+   }
+}
