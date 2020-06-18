bfcbdbc90a996a7a65110fe2353db5c142405ec2
barleegrow <bstacey@rta.org>
2020-06-18 16:24:55 -0400

    updates

---
+++ Week9/js/home.js
@@ -0,0 +1,40 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['towndata'];
+
+    //Create elements and content
+    for (let i = 0; i < towndata.length; i++ ) {
+        let card = document.createElement('section');
+        let photo = document.createElement('img');
+        let name = document.createElement('h1');
+        let motto = document.createElement('h4');
+        let founded = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+
+        //Create content
+        photo.setAttribute('src', 'images/' + towns[i].photo)
+        photo.setAttribute('alt', towns[i].name)
+        name.textContent = towns[i].name;
+        motto.textContent = '\"' + towns[i].motto +'\"';
+        founded.textContent = 'Founded: ' + towns[i].yearFounded;
+        polulation.textContent = 'Population: ' + towns[i].currentPopulation;
+        founded.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+        
+
+        //Put elements in each
+        card.appendChild(photo);
+        card.appendChild(name);
+        card.appendChild(motto);
+        card.appendChild(founded);
+        card.appendChild(population);
+        card.appendChild(rainfall);
+        document.querySelector('div.cards').appendChild(card);
+    }
+  });
