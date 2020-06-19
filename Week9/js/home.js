6fb9ade62f96f825e2c789d73d5846da8b90b80d
barleegrow <bstacey@rta.org>
2020-06-19 17:29:35 -0400

    push

--- Week9/js/home.js
+++ Week9/js/home.js
@@ -8,18 +8,21 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const towns = jsonObject['towns'];
 
-    //Create elements and content
+//Create elements and content 
     for (let i = 0; i < towns.length; i++ ) {
         if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven"){
+ //Create section image
         let card = document.createElement('section');
         let photo = document.createElement('img');
+//create text section
+        let text = document.createElement('text');
         let name = document.createElement('h1');
         let motto = document.createElement('h4');
         let founded = document.createElement('p');
         let population = document.createElement('p');
         let rainfall = document.createElement('p');
 
-        //Create content
+//Create content
         photo.setAttribute('src', 'towns/' + towns[i].photo)
         photo.setAttribute('alt', towns[i].name)
         name.textContent = towns[i].name;
@@ -29,13 +32,15 @@ fetch(requestURL)
         founded.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
         
 
-        //Put elements in each
+ //Put elements in image section
         card.appendChild(photo);
-        card.appendChild(name);
-        card.appendChild(motto);
-        card.appendChild(founded);
-        card.appendChild(population);
-        card.appendChild(rainfall);
+//put elements in text section
+        text.appendChild(name, motto, founded, population, rainfall)
+       // card.appendChild(name);
+      //  card.appendChild(motto);
+      //  card.appendChild(founded);
+      //  card.appendChild(population);
+     //   card.appendChild(rainfall);
         document.querySelector('.towns').appendChild(card);
         }
     }
