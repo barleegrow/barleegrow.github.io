e4c8716b991d0701c08a0726c032a97df5a503a9
barleegrow <bstacey@rta.org>
2020-06-22 19:28:34 -0400

    copy

--- week10/js/weatherapi.js
+++ week10/js/weatherapi.js
@@ -1,5 +1,5 @@
-        const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?id=' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb';
-        
+        const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?=' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb';
+      
         fetch(apiURL)
   .then((response) => response.json())
   .then((town) => {
