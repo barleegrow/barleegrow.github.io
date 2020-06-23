586e5bd9a22352761aa6b34765dad0e13b457596
barleegrow <bstacey@rta.org>
2020-06-22 20:55:54 -0400

    url

--- week10/js/weatherapi.js
+++ week10/js/weatherapi.js
@@ -1,14 +1,26 @@
-        const requestURL = 'http://api.openweathermap.org/data/2.5/forecast?=' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb';
+ /*Preston*/
+ const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb'
       
         fetch(apiURL)
   .then((response) => response.json())
   .then((town) => {
     console.log(town);
     let description = town.weather[0].description;
+
+    document.getElementById()
     
-
-
-
-
+/*5 day forecast*/
+function buildForecast(id)
+    const days = ['Sun', 'Mon', ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    url = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb'
 
   });
+
+/*Soda Springs
+
+/*5 day forecast*/
+
+/*Fish Haven
+
+
+/*5 day forecast*/
