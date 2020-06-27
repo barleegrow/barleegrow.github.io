14684dfb8d127d41a82f4292accbd3466f716d1a
barleegrow <bstacey@rta.org>
2020-06-26 20:39:57 -0400

    description image

--- week10/js/weatherapi.js
+++ week10/js/weatherapi.js
@@ -55,8 +55,9 @@ fivedayforecast.forEach(forecast => {
   let d= new Date(forecast.dt_txt);
   document.getElementById('data'+(day+1)).textContent = forecast.main.temp; 
   document.getElementById('col-head'+(day+1)).textContent = weekdays[d.getDay()];
-  document.getElementById('wicon'+(day+1)).src = "http://openweathermap.org/img/wn/" + forecast.weather[0].icon + "10d@2x.png";
+  document.getElementById('wicon'+(day+1)).src = "https://openweathermap.org/img/wn/" + forecast.weather[0].icon + "10d@2x.png";
   day++;
+  document.getElementById('wicon'+(day+1)).alt = forecast.weather[0].description;
 });
 
     
