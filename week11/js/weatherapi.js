0187d6ef12885f5cd6fa7c860dfd9a88a628f8a4
barleegrow <bstacey@rta.org>
2020-06-29 11:50:23 -0400

    5 day preston

--- week11/js/weatherapi.js
+++ week11/js/weatherapi.js
@@ -17,22 +17,28 @@
     document.getElementById('humid').innerHTML = Math.round(town.main.humidity) + "%";
     
 /*5 day forecast*/
-function buildForecast(id)
-  url = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473' + id + '&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb'
-    fetch(apiURL)
-    .then((response) => response.json())
-    .then((town) => {
-      const townList = town.list;
-      let daycount = 0;
+const apiURL2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=0b991a27171c24f0a1099f22e88cd0eb';
+fetch(apiURL2)
+.then((response) => response.json())
+.then((jsObject) => {
+console.log(jsObject);
+const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes ('18:00:00'));
 
-      for (let i = 0, i <town.list.length; i++){}
-    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
- 
-/*Soda Springs
+console.log(fivedayforecast)
 
-/*5 day forecast*/
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-/*Fish Haven
+let day = 0;
+fivedayforecast.forEach(forecast => {
+
+let d= new Date(forecast.dt_txt);
+document.getElementById('data'+(day+1)).textContent = forecast.main.temp; 
+document.getElementById('col-head'+(day+1)).textContent = weekdays[d.getDay()];
+document.getElementById('wicon'+(day+1)).src = "https://openweathermap.org/img/wn/" + forecast.weather[0].icon + "@2x.png";
+day++;
+document.getElementById('wicon'+(day+1)).alt = forecast.weather[0].description;
+});
 
 
-/*5 day forecast*/
+
+});
