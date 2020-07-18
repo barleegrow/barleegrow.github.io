058d70251fc2cc66cf6f25fa04677680d8650ffc
barleegrow <bstacey@rta.org>
2020-07-18 14:46:38 -0400

    scss

--- week12/js/rentals.js
+++ week12/js/rentals.js
@@ -1,4 +1,4 @@
-const requestURL = '/week12/data/rentals.json';
+const requestURL = 'https://barleegrow.github.io/week12/data/rentals.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -6,42 +6,50 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
+    const rentals = jsonObject['rentals'];
 
 //Create elements and content 
-    for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven"){
+    for (let i = 0; i < rentals.length; i++ ) {
+        if (rentals[i].name == "Honda Metro Scooter" || rentals[i].name == "Honda Dio Scooter" || rentals[i].name == "Honda PCX150 Scooter"
+        || rentals[i].name == "Honda Pioneer ATV" || rentals[i].name == "Jeep Wrangler - 4 door with a/c" || rentals[i].name == "Jeep Wrangler - 2 door"){
  //Create section image
         let card = document.createElement('section');
         let photo = document.createElement('img');
 //create text section 
         let text = document.createElement('div');
-        let name = document.createElement('h1');
-        let motto = document.createElement('h4');
-        let founded = document.createElement('p');
-        let population = document.createElement('p');
-        let rainfall = document.createElement('p');
+        let type = document.createElement('h4');
+        let max = document.createElement('p');
+        let halfR = document.createElement('p');       
+        let fullR = document.createElement('p');
+        let halfW = document.createElement('p');
+        let fullW = document.createElement('p');
 
 //Create content
-        photo.setAttribute('src', 'towns/' + towns[i].photo)
-        photo.setAttribute('alt', towns[i].name)
-        name.textContent = towns[i].name;
-        motto.textContent = '\"' + towns[i].motto +'\"';
-        founded.textContent = 'Founded: ' + towns[i].yearFounded;
-        population.textContent = 'Population: ' + towns[i].currentPopulation;
-        rainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-        
+        photo.setAttribute('src', 'rentals/' + rentals[i].picture)
+        photo.setAttribute('alt', rentals[i].name)
+        type.textContent = rentals[i].type;
+        max.textContent = rentals[i].max;
+        halfR.textContent = 'Half Day' + rentals[i].halfR;
+        fullR.textContent = 'Full Day ' + rentals[i].fullR;
+        halfW.textContent = 'Walk-in Half Day: ' + rentals[i].halfW;
+        fullW.textContent = 'Walk-n Full Day: ' + rentals[i].fullW;       
 
  //Put elements in image section
         card.appendChild(photo);
 //put elements in text section
-        text.appendChild(name);
-        text.appendChild(motto);
-        text.appendChild(founded);
-        text.appendChild(population);
-        text.appendChild(rainfall);
-        card.appendChild(text)
-        document.querySelector('.towns').appendChild(card);
+        text.appendChild(type);
+        text.appendChild(max);
+        text.appendChild(halfR);
+        text.appendChild(fullR);
+        text.appendChild(halfW);
+        card.appendChild(fullW)
+        document.querySelector('div.cards').appendChild(card);
         }
     }
   });
+
+  //confirmation number
+  function rand(maxLimit = 100) {
+        let rand = Math.random() * maxLimit;
+        return Math.floor(rand);
+  }
